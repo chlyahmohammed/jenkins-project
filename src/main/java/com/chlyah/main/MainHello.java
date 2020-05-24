5713f41f9217aa526f12649f00dcda7102e7ba8b
chlyahmohammed <chlyahmohammed@gmail.com>
2020-05-24 11:03:46 +0200

    first commit

---
+++ src/main/java/com/chlyah/main/MainHello.java
@@ -0,0 +1,10 @@
+package com.chlyah.main;
+
+public class MainHello {
+
+	public static void main(String[] args) {
+		System.out.println("hello");// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/chlyah/main/MainhelloTest.java
@@ -0,0 +1,14 @@
+package com.chlyah.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainhelloTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
